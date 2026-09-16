64adf76bba6c40dedcdf96148e471f0f619356e5
wello-e <mykmyk2137@gmail.com>
2026-09-16 16:18:42 +0200

    Implement service worker for caching assets

---
+++ script.js
@@ -0,0 +1,34 @@
+const CACHE = "kamila-filip-v1";
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE).then(cache => {
+
+            return cache.addAll([
+                "./",
+                "index.html",
+                "manifest.json"
+            ]);
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
